6070dcb63d5e29223d67ac0fb9dfe46f62c1a565
NACK2 <nicholaskang5@gmail.com>
2024-12-26 18:13:00 -0800

    set up prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import Home from './Home';
+import Home from "./Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
