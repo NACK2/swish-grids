f7bcf727f2ea8ba891368a16dc311936470bfbfb
NACK2 <nicholaskang5@gmail.com>
2024-12-27 22:57:24 -0800

    created api endpoint for retrieving players

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,21 @@
 import express from "express";
+import { Player, convertCSVtoJSON } from "./utils";
 
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
+});
+
+app.get("/players", async (_, res) => {
+  let players: Player[] = [];
+
+  try {
+    players = await convertCSVtoJSON("NBA_Player_IDs.csv");
+  } catch (err: any) {
+    console.error("Error: ", err.message);
+  }
+
+  res.status(200).json(players);
 });
