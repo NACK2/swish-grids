ac9e7e975405af8e949ddc4683d6d82e5c090b0f
NACK2 <nicholaskang5@gmail.com>
2024-12-28 23:52:07 -0800

    working on web scraping getTeams function

--- backend/utils.ts
+++ backend/utils.ts
@@ -1,11 +1,13 @@
 import csv from "csv-parser";
 import fs from "fs";
+import puppeteer from "puppeteer";
 
 export interface Player {
   NBAID: string;
   Name: string;
 }
 
+// given a csv file containing NBA player data, return array of objects of each player and their NBA ID
 // input: fileName: file name of CSV
 // output: array of nba player + id objects
 async function convertCSVtoJSON(fileName: fs.PathLike): Promise<Player[]> {
@@ -28,4 +30,16 @@ async function convertCSVtoJSON(fileName: fs.PathLike): Promise<Player[]> {
   });
 }
 
-export { convertCSVtoJSON };
+// given NBA player ID, return what teams they've played for
+// input: NBA ID
+// output: array of NBA teams played for
+async function getTeams(id: string): Promise<string[]> {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(`https://www.nba.com/stats/player/${id}`);
+
+  return Promise.resolve([]);
+}
+
+export { convertCSVtoJSON, getTeams };
