d1b0408a5af7cd13f5c6458bc8df52457ddca2f8
Nicholas Kang <nicholaskang5@gmail.com>
2025-01-01 22:36:45 -0800

    fixed auto refresh frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: "**/*.tsx",
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
